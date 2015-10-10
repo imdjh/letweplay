9fbfc4c13b8ffc90b4d4df57c0e2028d29a7eb0b
Jiahao Dai <20123225@student.shiep.edu.cn>
2015-10-10 20:36:57 +0800

    Fix export, add environ to forever

--- run.sh
+++ run.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-export  WEPLAY_PORT 3000
-export  WEPLAY_IO_URL "http://159.203.3.193:3001"
-export  WEPLAY_REDIS "159.203.3.193:6379"
-
-forever start /srv/weplay-web/index.js 
-tail -f root/.forever/*.log
+WEPLAY_PORT=3000 WEPLAY_IO_URL="http://159.203.3.193:3001" WEPLAY_REDIS="159.203.3.193:6379" forever /srv/weplay-web/index.js 
