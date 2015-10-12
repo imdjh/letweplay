4ba8be5cd149a8b762a78b03d98b6ca7780f4c14
Jiahao Dai <20123225@student.shiep.edu.cn>
2015-10-12 12:27:02 +0800

    Able to connect auth protected redis

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+# MAGICs
 export WEPLAY_PORT=3001
-export WEPLAY_WEB_PORT=3001
+export WEPLAY_WEB_PORT=3000
 export WEPLAY_IO_URL="http://localhost:3001"
-ENV WEPLAY_SERVER_UID 1337
-ENV WEPLAY_IP_THROTTLE 50
+export WEPLAY_SERVER_UID=1337
+export WEPLAY_IP_THROTTLE=50
 
-if [ ! -z "${REDIS_PORT_6379_TCP_ADDR}" ];then
-    export WEPLAY_PORT=${REDIS_PORT_6379_TCP_ADDR}:${REDIS_PORT_6379_TCP_PORT}
+if [[ -n "${REDIS_PORT}" ]];then
+    export WEPLAY_REDIS_URI=${REDIS_PORT_6379_TCP_ADDR}:${REDIS_PORT_6379_TCP_PORT}
+    export WEPLAY_REDIS_AUTH=${REDIS_PASSWORD}
+else
+    echo "Redis setting not found, can't start server." >&2 && exit 1
 fi
 
 
