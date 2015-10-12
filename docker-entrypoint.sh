a5599bdc92528bff9f7ab90dbe4f67f4567d1660
Jiahao Dai <20123225@student.shiep.edu.cn>
2015-10-12 12:54:08 +0800

    DEBUG environ varibles

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,6 +7,13 @@ export WEPLAY_IO_URL="http://localhost:3001"
 export WEPLAY_SERVER_UID=1337
 export WEPLAY_IP_THROTTLE=50
 
+echo "DEBUG: giving redis related value"
+echo "addr=> ${REDIS_PORT_6379_TCP_ADDR}:${REDIS_PORT_6379_TCP_PORT}"
+echo "auth=> ${REDIS_PASSWORD}"
+
+echo "DEBUG hard"
+env
+
 if [[ -n "${REDIS_PORT}" ]];then
     export WEPLAY_REDIS_URI=${REDIS_PORT_6379_TCP_ADDR}:${REDIS_PORT_6379_TCP_PORT}
     export WEPLAY_REDIS_AUTH=${REDIS_PASSWORD}
